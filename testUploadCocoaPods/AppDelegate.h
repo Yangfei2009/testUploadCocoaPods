de00dab75268ff983bee29b93b8d42d832679b4d
Yang <yangfei@jingge.cc>
2015-03-18 16:23:25 +0800

    第一次提交啊，

---
+++ testUploadCocoaPods/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testUploadCocoaPods
+//
+//  Created by Yang on 3/18/15.
+//  Copyright (c) 2015 JingGe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testUploadCocoaPods/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testUploadCocoaPods
+//
+//  Created by Yang on 3/18/15.
+//  Copyright (c) 2015 JingGe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
